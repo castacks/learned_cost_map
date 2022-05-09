c824e14ea10d30334b2aa57cf99c6de7880080a1
Mateo Guaman Castro <mguamanc@andrew.cmu.edu>
2022-05-09 10:36:39 -0400

    wandb docker command didn't work with slurm

--- cluster_scripts/train_slurm.sh
+++ cluster_scripts/train_slurm.sh
@@ -21,8 +21,4 @@ EXE_SCRIPT=$WORKING_DIR/train.sh
 
 USER=mguamanc
 
-# nvidia-docker run --rm --ipc=host -e CUDA_VISIBLE_DEVICES=`echo $CUDA_VISIBLE_DEVICES` -v /data/datasets:/data/datasets -v /home/$USER:/home/$USER -v /project:/project mguamanc/sara $EXE $EXE_SCRIPT
-
-# wandb docker-run run --rm --ipc=host -e CUDA_VISIBLE_DEVICES=`echo $CUDA_VISIBLE_DEVICES` -v /data/datasets:/data/datasets -v /home/$USER:/home/$USER -v /project:/project mguamanc/sara $EXE $EXE_SCRIPT
-
-wandb docker--rm --ipc=host -e CUDA_VISIBLE_DEVICES=`echo $CUDA_VISIBLE_DEVICES` -v /data/datasets:/data/datasets -v /home/$USER:/home/$USER -v /project:/project mguamanc/sara $EXE $EXE_SCRIPT
+nvidia-docker run --rm --ipc=host -e CUDA_VISIBLE_DEVICES=`echo $CUDA_VISIBLE_DEVICES` -v /data/datasets:/data/datasets -v /home/$USER:/home/$USER -v /project:/project mguamanc/sara $EXE $EXE_SCRIPT
