253a91bc7a7456f3c51aa7961ccb33a87ce1013a
Mateo Guaman Castro <mguamanc@andrew.cmu.edu>
2022-05-12 12:52:28 -0400

    Finished create_split file

--- scripts/learned_cost_map/trainer/create_split.py
+++ scripts/learned_cost_map/trainer/create_split.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 def create_split(num_train, num_val, all_train_fp, all_val_fp, output_dir):
     '''Creates two text files for a data split of a given size.
@@ -26,6 +27,7 @@ def create_split(num_train, num_val, all_train_fp, all_val_fp, output_dir):
     train_dict = {}
     with open(all_train_fp,'r') as f:
         train_lines = f.readlines()
+    # Sort training trajectories by length
     ind = 0
     while ind<len(train_lines):
         line = train_lines[ind].strip()
@@ -41,11 +43,75 @@ def create_split(num_train, num_val, all_train_fp, all_val_fp, output_dir):
         train_names.append(traj_name)
         train_lengths.append(traj_len)
         train_dict[traj_name] = frames
-    ## Sort trajectories by length
+    sorted_train_names = [x for _, x in sorted(zip(train_lengths, train_names))]
+    sorted_train_lengths = sorted(train_lengths)
 
-    ## Choose split from within the training trajectories, try to make them balanced by using sorted list
+    # Read in validation trajectories
+    val_names = []
+    val_lengths = []
+    val_dict = {}
+    with open(all_val_fp,'r') as f:
+        val_lines = f.readlines()
+    # Sort validation trajectories by length
+    ind = 0
+    while ind<len(val_lines):
+        line = val_lines[ind].strip()
+        traj_name, traj_len = line.split(' ')
+        traj_len = int(traj_len)
+        ind += 1
+        frames = []
+        for k in range(traj_len):
+            line = val_lines[ind].strip()
+            frames.append(line)
+            ind += 1
+
+        val_names.append(traj_name)
+        val_lengths.append(traj_len)
+        val_dict[traj_name] = frames
+    sorted_val_names = [x for _, x in sorted(zip(val_lengths, val_names))]
+    sorted_val_lengths = sorted(val_lengths)
+    # import pdb;pdb.set_trace()
+
+    ## Choose split from within the training trajectories
+    if num_train > len(train_names) or num_val > len(val_names):
+        raise NotImplementedError()
+    num_train = min(num_train, len(train_names))
+    num_val = min(num_val, len(val_names))
 
     ## Write text files in the given location
+
+    # Write training split
+    train_output_names = sorted_train_names[:num_train]
+    
+    train_split_fp = os.path.join(output_dir, f"train{num_train}.txt")
+    f = open(train_split_fp, 'w')
+
+    for i,traj_name in enumerate(train_output_names):
+        f.write(traj_name)
+        f.write(' ')
+        f.write(str(sorted_train_lengths[i]))
+        f.write('\n')
+        for ind in train_dict[traj_name]:
+            f.write(ind)
+            f.write('\n')
+    f.close()
+
+    # Write val split
+    val_output_names = sorted_val_names[:num_val]
+    
+    val_split_fp = os.path.join(output_dir, f"val{num_val}.txt")
+    f = open(val_split_fp, 'w')
+
+    for i,traj_name in enumerate(val_output_names):
+        f.write(traj_name)
+        f.write(' ')
+        f.write(str(sorted_val_lengths[i]))
+        f.write('\n')
+        for ind in val_dict[traj_name]:
+            f.write(ind)
+            f.write('\n')
+    f.close()
+
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
