b51834829e6ae716242a42cb0b42d58df23a71b7
Mateo Guaman Castro <mguamanc@andrew.cmu.edu>
2022-05-14 23:05:52 -0400

    Debugged models that include vel info

--- scripts/learned_cost_map/trainer/model.py
+++ scripts/learned_cost_map/trainer/model.py
@@ -26,18 +26,18 @@ class CostVelModel(nn.Module):
         self.model.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=64, kernel_size=(7,7), stride=(2,2), padding=(3,3), bias=False)
         self.model.fc = nn.Linear(in_features=512, out_features=embedding_size, bias=True)
 
-        self.vel_mlp = nn.Sequential([
+        self.vel_mlp = nn.Sequential(
             nn.Linear(in_features=1, out_features=512),
             nn.ReLU(),
             nn.Linear(in_features=512, out_features=512),
             nn.ReLU(),
             nn.Linear(in_features=512, out_features=512),
             nn.ReLU()
-        ])
+        )
 
-        self.output_mlp = nn.Sequential([
+        self.output_mlp = nn.Sequential(
             nn.Linear(in_features=embedding_size+512, out_features=output_size)
-        ])
+        )
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, input_data):
@@ -58,18 +58,18 @@ class CostFourierVelModel(nn.Module):
         self.model.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=64, kernel_size=(7,7), stride=(2,2), padding=(3,3), bias=False)
         self.model.fc = nn.Linear(in_features=512, out_features=embedding_size, bias=True)
 
-        self.vel_mlp = nn.Sequential([
+        self.vel_mlp = nn.Sequential(
             nn.Linear(in_features=ff_size, out_features=512),
             nn.ReLU(),
             nn.Linear(in_features=512, out_features=512),
             nn.ReLU(),
             nn.Linear(in_features=512, out_features=512),
             nn.ReLU()
-        ])
+        )
 
-        self.output_mlp = nn.Sequential([
+        self.output_mlp = nn.Sequential(
             nn.Linear(in_features=embedding_size+512, out_features=output_size)
-        ])
+        )
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, input_data):
