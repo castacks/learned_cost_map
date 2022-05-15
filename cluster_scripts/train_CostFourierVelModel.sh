6e82e7a35fb55acc2c995411dbd5dbc95addb290
Mateo Guaman Castro <mguamanc@andrew.cmu.edu>
2022-05-14 23:48:16 -0400

    Added bash scripts and slurm files to train models

---
+++ cluster_scripts/train_CostFourierVelModel.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+echo Running shell script that trains network with a given split.
+
+# Define python version
+EXE_PYTHON=python3
+
+# Define environment variables
+
+# Varibales to find code
+PACKAGE_DIR=/data/datasets/mguamanc/learned_cost_map
+BASE_DIR=/data/datasets/mguamanc/learned_cost_map/scripts/learned_cost_map/trainer
+
+# # Variables for generating data split:
+# PY_SPLIT=create_split.py
+# NUM_TRAIN=500
+# NUM_VAL=50
+# ALL_TRAIN_FP=/data/datasets/mguamanc/learned_cost_map/scripts/learned_cost_map/splits/tartandrive_train.txt
+# ALL_VAL_FP=/data/datasets/mguamanc/learned_cost_map/scripts/learned_cost_map/splits/tartandrive_val.txt
+# OUTPUT_DIR=/data/datasets/mguamanc/learned_cost_map/scripts/learned_cost_map/splits
+
+# Variables for trainer
+PY_TRAIN=train.py
+DATA_DIR=/project/learningphysics/tartandrive_trajs
+TRAIN_SPLIT=/data/datasets/mguamanc/learned_cost_map/scripts/learned_cost_map/splits/tartandrive_train.txt
+VAL_SPLIT=/data/datasets/mguamanc/learned_cost_map/scripts/learned_cost_map/splits/tartandrive_val.txt
+MODEL=CostFourierVelModel
+RUN_NAME=train_${MODEL}
+NUM_EPOCHS=50
+BATCH_SIZE=128
+SEQ_LENGTH=1
+LEARNING_RATE=0.001
+GAMMA=0.95
+EVAL_INTERVAL=1
+SAVE_INTERVAL=1
+NUM_WORKERS=10
+
+
+
+# Install learned_cost_map package
+cd $PACKAGE_DIR
+sudo pip3 install -e .
+
+# Login to Weights and Biases
+wandb login b47938fa5bae1f5b435dfa32a2aa5552ceaad5c6
+export WANDB_MODE=offline
+wandb init -p SARA
+
+# # Run split script
+# ${EXE_PYTHON} $BASE_DIR/$PY_SPLIT \
+#     --num_train $NUM_TRAIN \
+#     --num_val $NUM_VAL \
+#     --all_train_fp $ALL_TRAIN_FP \
+#     --all_val_fp $ALL_VAL_FP \
+#     --output_dir $OUTPUT_DIR
+
+echo Running standard split
+
+# Run trainer
+${EXE_PYTHON} $BASE_DIR/$PY_TRAIN \
+    --model $MODEL \
+    --data_dir $DATA_DIR \
+    --train_split $TRAIN_SPLIT \
+    --val_split $VAL_SPLIT \
+    --log_dir $RUN_NAME \
+    --num_epochs $NUM_EPOCHS \
+    --batch_size $BATCH_SIZE \
+    --seq_length $SEQ_LENGTH \
+    -lr $LEARNING_RATE \
+    --gamma $GAMMA \
+    --eval_interval $EVAL_INTERVAL \
+    --save_interval $SAVE_INTERVAL \
+    --num_workers $NUM_WORKERS\
+    --shuffle_train \
+    --multiple_gpus
+    # --shuffle_train
+
+echo Training tiny network shell script ends.
