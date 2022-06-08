26fb06d0b43926b3bd808810f4c0c5d976d3c01e
Mateo Guaman Castro <mguamanc@andrew.cmu.edu>
2022-06-07 20:09:06 -0400

    Modified CostVelModel to work with balanced data

--- cluster_scripts/train_CostVelModel_slurm.sh
+++ cluster_scripts/train_CostVelModel_slurm.sh
@@ -8,7 +8,7 @@
 #SBATCH -w calculon 
 #SBATCH --gres=gpu:1
 #SBATCH --mem=65536  # MB
-#SBATCH --job-name=train_CostVelModel_aug
+#SBATCH --job-name=train_CostVelModel_bal
 #SBATCH -o /home/mguamanc/job_%j.out
 #SBATCH -e /home/mguamanc/job_%j.err
 #SBATCH --mail-type=ALL # BEGIN, END, FAIL, ALL
