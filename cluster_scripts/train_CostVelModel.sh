48d28d4657ff8c8a56126b2d246ec7684adeef33
Mateo Guaman Castro <mguamanc@andrew.cmu.edu>
2022-06-12 21:37:08 -0400

    Small typo

--- cluster_scripts/train_CostVelModel.sh
+++ cluster_scripts/train_CostVelModel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo Training CostVelModel network with balanced data with a given split.
+echo Running shell script that trains network with a given split.
 
 # Define python version
 EXE_PYTHON=python3
@@ -8,32 +8,52 @@ EXE_PYTHON=python3
 # Define environment variables
 
 # Varibales to find code
-BASE_DIR=/home/mateo/phoenix_ws/src/learned_cost_map/scripts/learned_cost_map/trainer
+PACKAGE_DIR=/data/datasets/mguamanc/learned_cost_map
+BASE_DIR=/data/datasets/mguamanc/learned_cost_map/scripts/learned_cost_map/trainer
+
+# # Variables for generating data split:
+# PY_SPLIT=create_split.py
+# NUM_TRAIN=500
+# NUM_VAL=50
+# ALL_TRAIN_FP=/data/datasets/mguamanc/learned_cost_map/scripts/learned_cost_map/splits/tartandrive_train.txt
+# ALL_VAL_FP=/data/datasets/mguamanc/learned_cost_map/scripts/learned_cost_map/splits/tartandrive_val.txt
+# OUTPUT_DIR=/data/datasets/mguamanc/learned_cost_map/scripts/learned_cost_map/splits
 
 # Variables for trainer
 PY_TRAIN=train.py
-DATA_DIR=/home/mateo/Data/SARA/tartancost_data
-TRAIN_LC_DIR=lowcost_5k
-TRAIN_HC_DIR=highcost_10k
-VAL_LC_DIR=lowcost_val_1k
-VAL_HC_DIR=highcost_val_2k
+DATA_DIR=/project/learningphysics/tartandrive_trajs
+TRAIN_SPLIT=/data/datasets/mguamanc/learned_cost_map/scripts/learned_cost_map/splits/train_uniform.txt
+VAL_SPLIT=/data/datasets/mguamanc/learned_cost_map/scripts/learned_cost_map/splits/val_uniform.txt
 MODEL=CostVelModel
-FOURIER_SCALE=10.0
-RUN_NAME=train_${MODEL}_lr_3e-4_g_99e-1_bal_aug_l2_scale_${FOURIER_SCALE}_0
+RUN_NAME=train_${MODEL}_bal_aug_l2_0
 NUM_EPOCHS=50
-BATCH_SIZE=1024
+BATCH_SIZE=128
 SEQ_LENGTH=1
 LEARNING_RATE=0.0003
 WEIGHT_DECAY=0.0000001
-GAMMA=0.99
+GAMMA=0.95
 EVAL_INTERVAL=1
 SAVE_INTERVAL=1
 NUM_WORKERS=10
 
 
+
+# Install learned_cost_map package
+cd $PACKAGE_DIR
+sudo pip3 install -e .
+
 # Login to Weights and Biases
 wandb login b47938fa5bae1f5b435dfa32a2aa5552ceaad5c6
+export WANDB_MODE=offline
 wandb init -p SARA
+
+# # Run split script
+# ${EXE_PYTHON} $BASE_DIR/$PY_SPLIT \
+#     --num_train $NUM_TRAIN \
+#     --num_val $NUM_VAL \
+#     --all_train_fp $ALL_TRAIN_FP \
+#     --all_val_fp $ALL_VAL_FP \
+#     --output_dir $OUTPUT_DIR
 
 echo Running standard split
 
@@ -41,20 +61,21 @@ echo Running standard split
 ${EXE_PYTHON} $BASE_DIR/$PY_TRAIN \
     --model $MODEL \
     --data_dir $DATA_DIR \
+    --train_split $TRAIN_SPLIT \
+    --val_split $VAL_SPLIT \
     --log_dir $RUN_NAME \
-    --balanced_loader \
-    --train_lc_dir $TRAIN_LC_DIR \
-    --train_hc_dir $TRAIN_HC_DIR \
-    --val_lc_dir $VAL_LC_DIR \
-    --val_hc_dir $VAL_HC_DIR \
     --num_epochs $NUM_EPOCHS \
     --batch_size $BATCH_SIZE \
+    --seq_length $SEQ_LENGTH \
     -lr $LEARNING_RATE \
     --gamma $GAMMA \
     --weight_decay $WEIGHT_DECAY \
     --eval_interval $EVAL_INTERVAL \
     --save_interval $SAVE_INTERVAL \
     --num_workers $NUM_WORKERS\
-    --augment_data \
-    --fourier_scale $FOURIER_SCALE
+    --shuffle_train \
+    --multiple_gpus \
+    --shuffle_train \
     # --pretrained
+
+echo Training CostFourierVelModel network shell script ends.
