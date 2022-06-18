8851e4e1408a48a659f508c52e26bc957e7a8dbf
Mateo Guaman Castro <mguamanc@andrew.cmu.edu>
2022-06-17 22:57:12 -0400

    Changed dirs in cost labeler

--- cluster_scripts/cost_labeler.sh
+++ cluster_scripts/cost_labeler.sh
@@ -12,9 +12,9 @@ BASE_DIR=/data/datasets/mguamanc/learned_cost_map/scripts/learned_cost_map/data_
 
 PY_SCRIPT=cost_labeler.py
 
-DATA_DIR=/project/learningphysics/tartancost_wanda_traj0609
+DATA_DIR=/project/learningphysics/tartancost0531_traj
 
-COSTSTATS_DIR=/data/datasets/mguamanc/learned_cost_map/scripts/learned_cost_map/ros/wanda_cost_statistics.yaml
+COSTSTATS_DIR=/data/datasets/mguamanc/learned_cost_map/scripts/learned_cost_map/ros/cost_statistics.yaml
 
 
 # Install learned_cost_map package
