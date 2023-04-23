89c379925072737fa98682774b7f9ec8728ce189
Mateo Guaman Castro <mateo.guaman1998@gmail.com>
2023-04-23 19:45:05 -0400

    Slight modifications of slurm file

--- cluster_scripts/train_CostFourierVelModelBalancedSingularity_slurm.sh
+++ cluster_scripts/train_CostFourierVelModelBalancedSingularity_slurm.sh
@@ -4,9 +4,9 @@
 
 #SBATCH -n 10  # CPU Cores
 #SBATCH -t 1-00:00 # D-HH:MM
-#SBATCH -p a100-gpu-full # cpu/gpu/dgx 
+#SBATCH -p dgx1-gpu # cpu/gpu/dgx 
 #SBATCH --gres=gpu:1
-#SBATCH --mem=80G  # MB
+#SBATCH --mem=32G  # MB
 #SBATCH --job-name=train_CostFourierVelModel_balanced_singularity
 #SBATCH -o /home/mguamanc/job_%j.out
 #SBATCH -e /home/mguamanc/job_%j.err
@@ -24,4 +24,4 @@ source /etc/profile.d/modules.sh
 SIF="${SINGULARITY_DIR}/sara.sif"
 S_EXEC="singularity exec -B /data1:/data1 --nv ${SIF}"
 
-$S_EXEC $EXE $EXE_SCRIPT
+$S_EXEC $EXE_SCRIPT
