197829e603dd31f0d1609547ad6c5be1a6a898b5
Mateo Guaman Castro <guamanca@br012.ib.bridges2.psc.edu>
2023-04-24 12:34:14 -0400

    Small changes in slurm config

--- cluster_scripts/train_psc_slurm.sh
+++ cluster_scripts/train_psc_slurm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH -p GPU-shared
-#SBATCH -t 1-00:00
+#SBATCH -p GPU-small
+#SBATCH -t 05:00:00
 #SBATCH -n 5
 #SBATCH -J train_CostFourierVelModel_balanced_singularity
-#SBATCH --gpus=v100-16:1
+#SBATCH --gpus=v100-32:1
 #SBATCH -o /ocean/projects/cis220039p/guamanca/sbatch/outputs/learned_cost_map/job_%j.out
 #SBATCH -e /ocean/projects/cis220039p/guamanca/sbatch/outputs/learned_cost_map/job_%j.err
 
